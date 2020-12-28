307a791bab512c5dee9600643ae2eb54096e69c3
clyfe <>
2020-12-28 16:49:32 +0200

    Update for single mvc ns

--- src/tape/toasts/controller.cljs
+++ src/tape/toasts/controller.cljs
@@ -2,7 +2,7 @@
   "Toasts controller module."
   (:require
    [medley.core :as m]
-   [tape.mvc.controller :as c :include-macros true]
+   [tape.mvc :as mvc :include-macros true]
    [tape.tools.timeouts.controller :as timeouts.c]))
 
 ;;; Model
@@ -15,47 +15,47 @@
 (def ^:private keep-toast-ms 5000)
 
 (defn- timeout [toast]
-  {:ms      keep-toast-ms
-   :set     [::assoc-timeout-id toast]
+  {:ms keep-toast-ms
+   :set [::assoc-timeout-id toast]
    :timeout [::delete toast]})
 
 (defn- next-key [toasts]
   (let [keys (keys toasts)
-        mx   (apply max keys)]
+        mx (apply max keys)]
     (inc (or mx 0))))
 
 ;;; Events
 
 (defn create
-  {::c/reg ::c/event-fx}
+  {::mvc/reg ::mvc/event-fx}
   [{:keys [db]} [_ icon message]]
-  (let [key   (next-key (::toasts db))
+  (let [key (next-key (::toasts db))
         toast (make icon message key)]
-    {:db              (assoc-in db [::toasts key] toast)
+    {:db (assoc-in db [::toasts key] toast)
      ::timeouts.c/set (timeout toast)}))
 
 (defn assoc-timeout-id
-  {::c/reg ::c/event-db}
+  {::mvc/reg ::mvc/event-db}
   [db [_ toast timeout-id]]
   (m/update-existing-in db [::toasts (:key toast)]
                         assoc :timeout-id timeout-id))
 
 (defn set-timeout
-  {::c/reg ::c/event-fx}
+  {::mvc/reg ::mvc/event-fx}
   [_cofx [_ toast]]
   (when-not (some? (:timeout-id toast))
     {::timeouts.c/set (timeout toast)}))
 
 (defn clear-timeout
-  {::c/reg ::c/event-fx}
+  {::mvc/reg ::mvc/event-fx}
   [{:keys [db]} [_ toast]]
   (when-let [timeout-id (:timeout-id toast)]
     {::timeouts.c/clear timeout-id
-     :db                (m/update-existing-in db [::toasts (:key toast)]
-                                              dissoc :timeout-id)}))
+     :db (m/update-existing-in db [::toasts (:key toast)]
+                               dissoc :timeout-id)}))
 
 (defn delete
-  {::c/reg ::c/event-fx}
+  {::mvc/reg ::mvc/event-fx}
   [{:keys [db]} [_ toast]]
   (let [{:keys [key timeout-id]} toast]
     (cond-> {:db (m/dissoc-in db [::toasts key])}
@@ -64,10 +64,10 @@
 ;;; Subs
 
 (defn toasts
-  {::c/reg ::c/sub}
+  {::mvc/reg ::mvc/sub}
   [db _]
   (::toasts db))
 
 ;;; Module
 
-(c/defmodule)
+(mvc/defm ::module)
--- src/tape/toasts/view.cljs
+++ src/tape/toasts/view.cljs
@@ -1,13 +1,13 @@
 (ns tape.toasts.view
   (:require [cljsjs.react-transition-group]
             [re-frame.core :as rf]
-            [tape.mvc.view :as v :include-macros true]
+            [tape.mvc :as mvc :include-macros true]
             [tape.toasts.controller :as toasts.c]))
 
 ;;; Views
 
 (defn index
-  {::v/reg ::v/view}
+  {::mvc/reg ::mvc/view}
   []
   [:> js/ReactTransitionGroup.TransitionGroup {:class "toasts"}
    (for [[key toast] @(rf/subscribe [::toasts.c/toasts])
@@ -16,12 +16,12 @@
      [:> js/ReactTransitionGroup.CSSTransition
       {:in open :timeout 300 :class-names "toast"}
       [:div.notification.toast
-       {:class          (str "is-" (name icon))
-        :on-click       #(rf/dispatch [::toasts.c/delete toast])
+       {:class (str "is-" (name icon))
+        :on-click #(rf/dispatch [::toasts.c/delete toast])
         :on-mouse-enter #(rf/dispatch [::toasts.c/clear-timeout toast])
         :on-mouse-leave #(rf/dispatch [::toasts.c/set-timeout toast])}
        message]])])
 
 ;;; Module
 
-(v/defmodule)
+(mvc/defm ::module)
